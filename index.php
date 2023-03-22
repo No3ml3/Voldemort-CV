ca9bcc186b0bf9e5401097851688edcac38d08eb
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-03-22 14:13:53 +0100

    update contact security

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+    $messages_erreur = array();
+    if($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        $erreur = false;
+        foreach($_POST as $key => $value) {
+            $contact[$key] = trim($value);
+        }
+        // Vérification du champ "nom"
+        if (empty($contact['name'])) {
+            $erreur = true;
+            $messages_erreur['name'] = "Le champ 'Prénom' est obligatoire";
+        } 
+        
+        
+        
+        // Vérification du champ "email"
+        if (empty($contact['mail'])) {
+            $erreur = true;
+            $messages_erreur['mail'] = "Le champ 'Adresse e-mail' est obligatoire";
+        } elseif (!filter_var($contact['mail'], FILTER_VALIDATE_EMAIL)) {
+            $erreur = true;
+            $messages_erreur['mail'] = "L'adresse e-mail n'est pas valide";
+        }
+        
+        // Vérification du champ "message"
+        if (empty($contact['commentaire'])) {
+            $erreur = true;
+            $messages_erreur['commentaire'] = "Le champ 'commentaire' est obligatoire";
+        } 
+    }else{
+        echo 'votre message a bien été envoyer ';
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -29,122 +63,154 @@
     
 
 <!--la Modal--> 
-<div id="myModal" class="modal">
+    <div id="myModal" class="modal">
 
-    <!-- Modal contenu -->
-    <div id='modal-content' class="modal-content">
-    
-        <span onclick="closeModal()" class="close">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-        </svg>
-        </span>
-        <div id='presentation' class='modalPresentation'>
-            <h1>Présentation</h1>
-
-            <img src="assets/images/snake1.png" alt="snake">
-
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.
-            </p>
-        </div>
-
-
-        <div id='formation' class='modalFormation'>
+        <!-- Modal contenu -->
+        <div id='modal-content' class="modal-content">
         
-        <img src="assets/images/snake1.png" alt="snake">
-        <h1>Formations</h1>
-    
+            <span onclick="closeModal()" class="close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+            </svg>
+            </span>
+            <div id='presentation' class='modalPresentation'>
+                <h1>Présentation</h1>
 
-    <ul>
-        <li><span class="dates">1930 - 1933</span> : Ecole maternelle Ruben Koyekoué - Mantes-La-Jolie </li>
-        <li><span class="dates">1933 - 1939</span> : Ecole élémentaire Sylvain Blondeau - Bouzonville-Aux-Bois</li>
-        <li><span class="dates">1939 - 1950</span> : Ecole de magie et de sorcellerie - Poudlard</li>
-        <li><span class="dates">1952 - 1953</span> : Brevet d'aptitude aux fonctions d'animateur (BAFA) - Glasgow </li>
-        <li><span class="dates">1953 - 1955</span> : DUT Thanatopraxie : Université Amaury Becker - Edimbourg</li>
-        <li><span class="dates">1955 - 1957</span> : CQP d'agent technique en marbrerie funéraire - Ecole de Funétique - Dundee </li>
-    </ul>
-        </div>
-
-        <div id='competence' class='modalCompetence' >
-            <h1>Compétences</h1>
-            <img src="assets/images/snake1.png" alt="snake">
-            <p>
-
-            </p>
-        </div>
-
-        <div id='experience' class='modalExperience'>
-                
-                
-                <h1>Expériences</h1>
                 <img src="assets/images/snake1.png" alt="snake">
-                
+        
+                <p> 
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.
+                </p>
+            </div>
 
+
+            <div id='formation' class='modalFormation'>
+            
+            <img src="assets/images/snake1.png" alt="snake">
+            <h1>Formations</h1>
+        
+
+        <ul>
+            <li><span class="dates">1930 - 1933</span> : Ecole maternelle Ruben Koyekoué - Mantes-La-Jolie </li>
+            <li><span class="dates">1933 - 1939</span> : Ecole élémentaire Sylvain Blondeau - Bouzonville-Aux-Bois</li>
+            <li><span class="dates">1939 - 1950</span> : Ecole de magie et de sorcellerie - Poudlard</li>
+            <li><span class="dates">1952 - 1953</span> : Brevet d'aptitude aux fonctions d'animateur (BAFA) - Glasgow </li>
+            <li><span class="dates">1953 - 1955</span> : DUT Thanatopraxie : Université Amaury Becker - Edimbourg</li>
+            <li><span class="dates">1955 - 1957</span> : CQP d'agent technique en marbrerie funéraire - Ecole de Funétique - Dundee </li>
+        </ul>
+            </div>
+
+            <div id='competence' class='modalCompetence' >
+                <h1>Compétences</h1>
+                <img src="assets/images/snake1.png" alt="snake">
+                <p>
+
+                </p>
+            </div>
+
+            <div id='experience' class='modalExperience'>
+                    
+                    
+                    <h1>Expériences</h1>
+                    <img src="assets/images/snake1.png" alt="snake">
+                    
+
+                <ul>
+                    <li><span class="dates">1958 - 1960</span> <p> Animateur en centre de loisirs - Les Sequoias - Edimbourg</p> </li>
+                    <li><span class="dates">1960 - 1964</span> <p> Vendeur - Barjow & Beurk - Allée des Embrumes - Londres</p></li>
+                    <li><span class="dates">1964 - 1969</span> <p> Chauffeur-livreur - Chronopost - Saran</p></li>
+                    <li><span class="dates">1969 - 1972</span> <p> Equarrisseur - Coopérative des trois moutons - Glasgow</p></li>
+                    <li><span class="dates">1972 - 1975</span> <p> Professeur de Magie Noire - Poudlard</p> </li>
+                    <li><span class="dates">1975 - 1987</span> <p> Commis de cuisine - Fouquet's - Paris</p> </li>
+                </ul>
+            </div>
+
+            <div id='contact'  class='modalContact'>
             <ul>
-                <li><span class="dates">1958 - 1960</span> <p> Animateur en centre de loisirs - Les Sequoias - Edimbourg</p> </li>
-                <li><span class="dates">1960 - 1964</span> <p> Vendeur - Barjow & Beurk - Allée des Embrumes - Londres</p></li>
-                <li><span class="dates">1964 - 1969</span> <p> Chauffeur-livreur - Chronopost - Saran</p></li>
-                <li><span class="dates">1969 - 1972</span> <p> Equarrisseur - Coopérative des trois moutons - Glasgow</p></li>
-                <li><span class="dates">1972 - 1975</span> <p> Professeur de Magie Noire - Poudlard</p> </li>
-                <li><span class="dates">1975 - 1987</span> <p> Commis de cuisine - Fouquet's - Paris</p> </li>
-            </ul>
+                    <?php
+                        if ( isset($erreur) && $erreur === true )
+                        {
+                            foreach ($messages_erreur as $champ => $message) { ?>
+                            <li> <?php  echo 'Erreur dans le champ ' . $champ . ':' . $message;?> </li><br>
+                            
+                    <?php } }?>
+                <form  action="" method="POST">
+
+                    <legend>Contact</legend>
+                
+                        <label for="name">Votre Prénom</label>
+                        <input type="text" id="name"  name='name' value='' required></input>
+                    
+                
+                        <label for="email">Votre e-mail</label>
+                        <input type="email" id="email" name='mail'  required></input>
+                    
+                
+                        <label for="commentaire"> Votre commentaire</label>
+                        <textarea name="commentaire" id="commentaire"  required></textarea>
+                    
+                    <button type='submit'>Submit</button>
+
+                </form>
+                
+                    
+
+                </ul>
+            </div>
+
+            <div id='reference' class='modalReference'>
+                <h1>Références</h1>
+                <img src="assets/images/snake1.png" alt="snake">
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.
+                </p>
+            </div>
+
+            <div id='information' class='modalInformation'>
+                <h1>Informations</h1>
+                <img src="assets/images/snake1.png" alt="snake">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
+                    et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
+                    id eveniet? Praesentium corporis doloribus molestias.
+                </p>
+            </div>
         </div>
-
-        <div id='contact'  class='modalContact'>
-            <form method="post" action="form.php">
-
-                <legend>Contact</legend>
-                <label for="name"></label>
-                <input type="text" id="name" placeholder="Votre nom..."></input>
-
-                <label for="email"></label>
-                <input type="email" id="email" placeholder="Votre e-mail..."></input>
-
-                <label for="commentaire"></label>
-                <textarea name="commentaire" id="commentaire" placeholder="Votre commentaire..."></textarea>
-                <button><a href="#">Submit</a></button>
-
-            </form>
-        </div>
-
-        <div id='reference' class='modalReference' >
-            <h1>Références</h1>
-            <img src="assets/images/snake1.png" alt="snake">
-
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.
-            </p>
-        </div>
-
-        <div id='information' class='modalInformation'>
-            <h1>Informations</h1>
-            <img src="assets/images/snake1.png" alt="snake">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur officiis praesentium explicabo
-                et eum maxime delectus inventore ducimus alias, accusamus libero voluptates possimus quidem
-                id eveniet? Praesentium corporis doloribus molestias.
-            </p>
-        </div>
-    </div>
-</div>    
+    </div>    
 
 
 
     
 </body>
 <script src="script.js"></script>
+<script>
+    if( <?php echo ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($messages_erreur)) ? true : false ?> ){
+        modal.style.display = "block";
+        content.style.display = "block";
+        modalTextContact.style.display = "block";
+        audio.play();
+        modalTextFormation.style.display = "none";
+        modalTextCompetence.style.display = "none";
+        modalTextExperience.style.display = "none";
+        modalTextPresentation.style.display = "none";
+        modalTextReference.style.display = "none";
+        modalTextInformation.style.display = "none";
+    }
+
+</script>
 </html>

5379f7bb14e12858e031aadf6d9999d9c3d53f64
jaalazard <ja.alazard@proton.me>
2023-03-22 15:46:09 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhh

---
+++ dataRef.php
@@ -0,0 +1,38 @@
+<?php
+
+$dataReferences = [
+    'references' => [
+        [
+            'name' => 'Judas',
+            'job' => 'Traître',
+            'picture' => 'judas.png'
+        ],
+        [
+            'name' => 'Hannibal Lecter',
+            'job' => 'Psychiatre - Tueur en série',
+            'picture' => 'hannibal.webp',
+        ],
+        [
+            'name' => 'Sir Amaury "Steeve Jobs" Becker',
+            'job' => 'Inventeur du cyanure',
+            'picture' => 'amauryBecker.png',
+        ],
+        [
+            'name' => 'Satan',
+            'job' => 'C.E.O des Enfers',
+            'picture' => 'diablo.jpg',
+        ],
+        [
+            'name' => 'Sauron',
+            'job' => 'Seigneur des ténèbres - Maître des Nazgûl',
+            'picture' => 'sauron.jpg',
+        ],
+        [
+            'name' => 'Vladimir Vladimirovitch Poutine',
+            'job' => 'Président de la fédération de Russie',
+            'picture' => 'vladimir.jpg',
+        ],
+    ],
+];
+
+?>

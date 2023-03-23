dc92b55935e6ed38bf12fbd97b29ce7c6f987e0a
jaalazard <ja.alazard@proton.me>
2023-03-23 10:34:12 +0100

    modal formations

---
+++ dataFormations.php
@@ -0,0 +1,39 @@
+<?php
+
+$dataFormations = [
+    'formations' => [
+        [
+            'date' => '1930 - 1933',
+            'school' => 'Ecole maternelle Ruben Koyekoué',
+            'town' => 'Mantes-La-Jolie'
+        ],
+        [
+            'date' => '1933 - 1939',
+            'school' => 'Ecole élémentaire Sylvain Blondeau',
+            'town' => 'Bouzonville-Aux-Bois'
+        ],
+        [
+            'date' => '1939 - 1950',
+            'school' => 'Ecole de magie et de sorcellerie',
+            'town' => 'Poudlard'
+        ],
+        [
+            'date' => '1952 - 1953',
+            'school' => 'Brevet d\'aptitude aux fonctions d\'animateur (BAFA)',
+            'town' => 'Glasgow'
+        ],
+        [
+            'date' => '1953 - 1955',
+            'school' => 'DUT Thanatopraxie : Université Amaury Becker',
+            'town' => 'Edimbourg'
+        ],
+        [
+            'date' => '1955 - 1957',
+            'school' => 'CQP Agent technique en marbrerie funéraire - Ecole de Funétique',
+            'town' => 'Dundee'
+        ],
+        
+    ],
+];
+
+?>

b266dfe95e4103b5480bc01445f48f60db0a6fc4
Noemie.BARRE <noemie.barre24@gmail.com>
2023-03-23 10:40:52 +0100

    change design

---
+++ tableauInformation.php
@@ -0,0 +1,28 @@
+<?php
+$dataInformation = [
+    'Loisir' => [
+        'Tuer des gens',
+        'Club mangemort',
+        'Les reptiles'
+    ],
+    'Défault' => [
+        'narcissique',
+        'sociopathe',
+        'mégalomane',
+        'alexithymie',
+        'assassin',
+    ],
+    
+    'Objectifs' => [
+        'Dominer le monde',
+        'Détruire les moldus',
+        'Tuer Harry Potter',
+    ],
+   
+    'Langue' => [
+        'Anglais',
+        'Fourchelang',
+    ],
+   ];
+
+?>
